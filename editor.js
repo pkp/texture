6fa041016143b58a308e8b0ba5283c4449a60b6c
Dulip Withanage <dulip.withanage@gmail.com>
2018-11-06 14:02:52 +0100

    [Update] Substance Texture V 1.0 https://github.com/asmecher/texture/issues/6

--- editor.js
+++ editor.js
@@ -1,113 +1,42 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('substance'), require('substance-texture')) :
-	typeof define === 'function' && define.amd ? define(['substance', 'substance-texture'], factory) :
-			(factory(global.window.substance,global.window.texture));
-}(this, (function (substance,substanceTexture) {
-	window.addEventListener('load', () => {
-		substance.substanceGlobals.DEBUG_RENDERING = substance.platform.devtools;
-		let app = OJSTextureEditor.mount({}, window.document.body);
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('substance'), require('substance-texture')) :
+  typeof define === 'function' && define.amd ? define(['substance', 'substance-texture'], factory) :
+  (factory(global.substance,global.texture));
+}(this, (function (substance,substanceTexture) { 'use strict';
 
-		// put the archive and some more things into global scope, for debugging
-		setTimeout(() => {
-			window.app = app;
-		}, 500);
-	});
+  window.addEventListener('load', () => {
+    substance.substanceGlobals.DEBUG_RENDERING = substance.platform.devtools;
+    setTimeout(() => {
+      let app = DevWebApp.mount({
+        archiveId: substance.getQueryStringParam('archive') || 'kitchen-sink',
+        storageType: substance.getQueryStringParam('storage') || 'vfs',
+        storageUrl: substance.getQueryStringParam('storageUrl') || '/archives'
+      }, window.document.body);
 
-	// This uses a monkey-patched VfsStorageClient that checks immediately
-	// if the stored data could be loaded again, or if there is a bug in
-	// Textures exporter
-	class OJSTextureEditor extends substanceTexture.TextureWebApp {
-		didMount() {
-			this._init();
-			substance.DefaultDOMElement.getBrowserWindow().on('keydown', this._keyDown, this);
-		}
-		
-		dispose() {
-			substance.DefaultDOMElement.getBrowserWindow().off(this);
-		}
-		
-		getInitialState() {
-			return {
-				archive: undefined,
-				error: undefined
-			}
-		}
-		
-		_init() {
-			let storageUrl = document.querySelector('meta[name=jobId').getAttribute('content');
-			let storage = new substance.HttpStorageClient(storageUrl);
-			let buffer = new substance.InMemoryDarBuffer();
-			let archive = new substanceTexture.TextureArchive(storage, buffer);
-			window.archive = archive
-			window.buffer = buffer
+      // put the archive and some more things into global scope, for debugging
+      setTimeout(() => {
+        window.app = app;
+      }, 500);
+    });
+  });
 
-			let promise = archive.load('')
-			.then(() => {
-				setTimeout(() => {
-					this.setState({archive});
-				}, 0);
-			});
+  // This uses a monkey-patched VfsStorageClient that checks immediately
+  // if the stored data could be loaded again, or if there is a bug in
+  // Textures exporter
+  class DevWebApp extends substanceTexture.TextureWebApp {
+    _getStorage (storageType) {
+      let storage = super._getStorage(storageType);
+      if (storageType === 'vfs') {
+        substanceTexture.vfsSaveHook(storage, substanceTexture.TextureArchive);
+      }
+      return storage
+    }
 
-			if (!substance.platform.devtools) {
-				promise.catch(error => {
-					console.error(error);
-					this.setState({error});
-				});
-			}
-		}
-		
-		_keyDown(event) {
-			if ( event.key === 'Dead' ) return
-			// Handle custom keyboard shortcuts globally
-			let archive = this.state.archive;
-			if (archive) {
-				let manuscriptSession = archive.getEditorSession('manuscript');
-				let handled = manuscriptSession.keyboardManager.onKeydown(event);
-				if (!handled) {
-					let key = substance.parseKeyEvent(event);
-					// CommandOrControl+S
-					if (key === 'META+83' || key === 'CTRL+83') {
-						this._save();
-						event.preventDefault();
-					}
-				}
-			}
-		}
-		
-		_save() {
-			this.state.archive.save().then(() => {
-				console.info('successfully saved');
-			}).catch(err => {
-				console.error(err);
-			});
-		}
-		
-		render($$) {
-			let el = $$('div').addClass('sc-app');
-			let archive = this.state.archive;
-			let err = this.state.error;
+    _getArticleConfig () {
+      return substanceTexture.EditorPackage
+    }
+  }
 
-			if (archive) {
-				el.append(
-					$$(substanceTexture.Texture, {archive})
-				);
-			} else if (err) {
-				if (err.type === 'jats-import-error') {
-					el.append(
-							$$(substanceTexture.JATSImportDialog, { errors: err.detail })
-					);
-				} else {
-					el.append(
-						'ERROR:',
-						err.message
-					);
-				}
-			} else {
-			// LOADING...
-			}
-			return el
-		}
-	}
 })));
 
 //# sourceMappingURL=./editor.js.map
